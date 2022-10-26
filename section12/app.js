cb3a756672060d2f092690b29adf9fd0b63b1f90
sunkeydokey <sunkeydokey@gmail.com>
2022-10-26 21:37:17 +0900

    applied prettier in html

--- section12/app.js
+++ section12/app.js
@@ -7,14 +7,19 @@
 // anchorElement = document.querySelector("p a"); 첫 번째 요소만 반환
 // anchorElement.href = "https://github.com/sunkeydokey";
 
-let anchorElement = document.querySelectorAll("p a"); // 모든 요소를 배열로 반환
-console.log(anchorElement);
-anchorElement[0].href = "https://github.com/sunkeydokey";
+// let anchorElement = document.querySelectorAll('p a'); // 모든 요소를 배열로 반환
+// console.log(anchorElement);
+// anchorElement[0].href = 'https://github.com/sunkeydokey';
 
 // ADD AN ELEMENT
 // 1. Create the new element
 
-document.createElement("a");
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://github.com/sunkeydokey';
+newAnchorElement.textContent = 'My Github';
 
 // 2. Get access to the parent element that should hold the new element
+let firstPargraph = document.querySelector('p');
+
 // 3. Insert the new element into the parent element content
+firstPargraph.append(newAnchorElement);
