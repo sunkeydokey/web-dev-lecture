15f5d387d96e2e8d87d40a35218888d07a9e3bf8
sunkeydokey <sunkeydokey@gmail.com>
2022-11-02 01:56:59 +0900

    ended section 19, 20

--- nodejs-practice/app.js
+++ nodejs-practice/app.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
+const defaultRoutes = require('./routes/default');
+const resRoutes = require('./routes/restaurants');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -9,49 +12,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.use('/', defaultRoutes);
+app.use('/', resRoutes);
+
+app.use(function (req, res) {
+  res.status(404).render('404');
 });
 
-app.get('/restaurants', function (req, res) {
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
+app.use(function (error, req, res, next) {
+  res.status(500).render('500');
 });
-
-app.get('/restaurants/:id', function (req, res) {
-  const restaurantId = req.params.id;
-  res.render('restaurant-detail', { rid: restaurantId });
-});
-
-app.get('/recommend', function (req, res) {
-  res.render('recommend');
-});
-
-app.post('/recommend', function (req, res) {
-  const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-  res.redirect('/confirm');
-});
-
-app.get('/confirm', function (req, res) {
-  res.render('confirm');
-});
-
-app.get('/about', function (req, res) {
-  res.render('about');
-});
-
-app.get('');
 
 app.listen(3000);
